ed37e31d046f9bf6eecdd6a16fdf71fb3de46f40
Vitor Gabriel Blasques Victor <vitorvictorblasq@gmail.com>
2025-06-20 08:31:14 -0300

    fix: correção de nomes

--- src/components/Pages/Grupos/schema.ts
+++ src/components/Pages/Grupos/schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const formSchema = z.object({
   descricao: z
     .string({
-      required_error: "A descrição do centro de custo é obrigatória",
+      required_error: "A descrição do grupo é obrigatória",
     })
     .min(4, "A descrição deve ter mais de 3 caracteres.")
     .max(255, "A descrição deve ter menos de 255 caracteres."),
